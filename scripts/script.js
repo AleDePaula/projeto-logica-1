213c2a73c73b1c2171999237f343b373a1d33914
Alexandre de Paula <alexandreaugusto@gmail.com>
2024-04-30 09:47:52 -0300

    refac: refatorado o codigo para usar switches e cases ao invés de if e else if (estudo)

--- scripts/script.js
+++ scripts/script.js
@@ -11,32 +11,46 @@ let lutadores = [];
 
 function addHeroi(nome, lvl){
     
-    if (nome !== "" && lvl!==""){
+    if (nome !== "" && lvl !== "") {
         let rank;
-        if (lvl>=0 && lvl<=1000){
-            rank = "1"            
-        } else if (lvl>=1001 && lvl<=2000){
-            rank = "2"            
-        }else if (lvl>=2001 && lvl<=5000){
-            rank = "3"            
-        }else if (lvl>=5001 && lvl<=10000){
-            rank = "4"            
-        }else if (lvl>=10001 && lvl<=20000){
-            rank = "5"            
-        }else if (lvl>=20001 && lvl<=50000){
-            rank = "6"            
-        }else if (lvl>=50001 && lvl<=100000){
-            rank = "7"            
-        }else if (lvl>=100001){
-            rank = "8"
-        } 
-        var heroi = [nome,rank]
-        powerLevel.value=""
-        lutadores.push(heroi)
-        nomeInput.value=""
-        
+        switch (true) {
+            case lvl >= 0 && lvl <= 1000:
+                rank = "1";
+                break;
+            case lvl >= 1001 && lvl <= 2000:
+                rank = "2";
+                break;
+            case lvl >= 2001 && lvl <= 5000:
+                rank = "3";
+                break;
+            case lvl >= 5001 && lvl <= 10000:
+                rank = "4";
+                break;
+            case lvl >= 10001 && lvl <= 20000:
+                rank = "5";
+                break;
+            case lvl >= 20001 && lvl <= 50000:
+                rank = "6";
+                break;
+            case lvl >= 50001 && lvl <= 100000:
+                rank = "7";
+                break;
+            case lvl >= 100001:
+                rank = "8";
+                break;
+            default:
+                rank = null;
+        }
+        if (rank !== null) {
+            var heroi = [nome, rank];
+            powerLevel.value = "";
+            lutadores.push(heroi);
+            nomeInput.value = "";
+        } else {
+            alert('adicione valores válidos');
+        }
     } else {
-        alert('adicione valores validos')
+        alert('adicione valores válidos');
     }
 }
 
